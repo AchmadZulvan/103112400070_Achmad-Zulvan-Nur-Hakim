4c24420c60ccb7cf3e4f3ad488a4a5d068364f47
Achmad Zulvan Nur Hakim <zulvanachmad0@gmail.com>
2025-12-23 09:04:43 +0700

    update ode

--- Asessment2/GRAPH/main.cpp
+++ Asessment2/GRAPH/main.cpp
@@ -3,26 +3,18 @@
 using namespace std;
 
 int main() {
-    Graph vaccineNet;
-    initGraph(vaccineNet);
-
-    insertNode(vaccineNet, 'A');
-    insertNode(vaccineNet, 'B');
-    insertNode(vaccineNet, 'C');
-    insertNode(vaccineNet, 'D');
-    insertNode(vaccineNet, 'E');
-    addEdge(vaccineNet, 'A', 'B');
-    addEdge(vaccineNet, 'B', 'E');
-    addEdge(vaccineNet, 'B', 'C');
-    addEdge(vaccineNet, 'C', 'D');
-
-    cout << "Membangun Jaringan Distribusi Vaksin\n";
-    cout << "Node A terhubung ke: B\n";
-    cout << "Node B terhubung ke: E C A\n";
-    cout << "Node C terhubung ke: D B\n";
-    cout << "Node D terhubung ke: C\n";
-    cout << "Node E terhubung ke: B\n";
-    checkCriticalCities(vaccineNet);
+    Graph G;
+    initGraph(G);
+    insertNode(G, 'A');
+    insertNode(G, 'B');
+    insertNode(G, 'C');
+    insertNode(G, 'D');
+    insertNode(G, 'E');
+    addEdge(G, 'A', 'B');
+    addEdge(G, 'B', 'E');
+    addEdge(G, 'B', 'C');
+    addEdge(G, 'C', 'D');
+    checkCriticalCities(G);
 
     return 0;
 }
